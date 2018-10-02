afdd58b273666666c61656c7633e75ef7598d2cf
FrankSauve <franksauve@outlook.com>
2018-10-02 16:45:34 -0400

    Added dummy unit test for travis

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
