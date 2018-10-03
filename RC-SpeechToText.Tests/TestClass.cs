5d05cbc2909512e01dfbae8381cb8dd0283b2fd4
FrankSauve <franksauve@outlook.com>
2018-10-03 19:18:41 -0400

    Travis should work now

---
+++ RC-SpeechToText.Tests/TestClass.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC_SpeechToText.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.That(1 + 1 == 2);
+        }
+    }
+}
