22d262bffc4c11d7032ea0752262e84b51e73e13
FrankSauve <franksauve@outlook.com>
2018-10-08 18:59:34 -0400

    Changed from nunit to MStest

---
+++ RC-SpeechToText.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RC_SpeechToText.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 + 1 == 2);
+        }
+    }
+}
