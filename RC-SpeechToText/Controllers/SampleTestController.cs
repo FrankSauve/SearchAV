6605dff66e1929dea50241d3561659e6cb34c52d
François Crispo-Sauvé <f_crsipo@encs.concordia.ca>
2018-11-06 15:28:00 -0500

    Moved conversion to a utils folder instead of controllers #20

--- RC-SpeechToText/Controllers/SampleTestController.cs
+++ RC-SpeechToText/Controllers/SampleTestController.cs
@@ -6,11 +6,7 @@ using Google.Cloud.Speech.V1;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.IO;
-using MediaToolkit;
-using MediaToolkit.Model;
-using MediaToolkit.Options;
-using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
+
 
 namespace RC_SpeechToText.Controllers
 {
@@ -25,7 +21,7 @@ namespace RC_SpeechToText.Controllers
         [HttpPost("[action]")]
         public GoogleResult GoogleSpeechToText(IFormFile audioFile, IFormFile srtFile)
         {
-            
+
             var speech = SpeechClient.Create();
             var response = speech.Recognize(new RecognitionConfig()
             {
@@ -43,63 +39,6 @@ namespace RC_SpeechToText.Controllers
             };
 
             return googleResult;
-        }
-        /// <summary>
-        /// Converts an mp4 video into a flac audio file
-        /// GET: /api/sampletest/videotoaudio
-        /// </summary>
-        /// <returns>string</returns>
-        // TODO: Convert FLAC output file to mono
-        [HttpGet("[action]")]
-        public string VideoToAudio() 
-        {
-            var inputLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.mp4";
-            var outputLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.wav";
-            
-            var inputFile = new MediaFile { Filename = inputLocation};
-            var outputFile = new MediaFile { Filename = outputLocation };
-
-            var conversionOptions = new ConversionOptions
-            {
-                AudioSampleRate = AudioSampleRate.Hz22050
-            };
-
-            try
-            {
-                using (var engine = new Engine())
-                {
-                    engine.GetMetadata(inputFile);
-
-                    engine.Convert(inputFile, outputFile, conversionOptions);
-                }
-            }
-            catch (Exception ex)
-            {
-                return "Conversion Unsuccessful";
-            }
-            StereoToMono(outputLocation);
-            return "Conversion successful. Output file at: "+ outputLocation;
-        }
-        public String StereoToMono(String outputLocation)
-        {
-            var outputLocationMono = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan_Mono.wav";
-            var outputWaveLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.wav";
-            try
-            {
-                using (var inputReader = new AudioFileReader(outputLocation))
-                {
-                    var mono = new StereoToMonoSampleProvider(inputReader);
-                    mono.LeftVolume = 0.0f;
-                    mono.RightVolume = 1.0f;
-                    WaveFileWriter.CreateWaveFile16(outputLocationMono, mono);
-                }
-            }
-            catch (Exception ex)
-            {
-                return "Conversion Unsuccessful";
-            }
-            System.IO.File.Delete(outputWaveLocation);
-            return "Conversion from stereo to mono successful.";
         }
     }
 
---
+++ RC-SpeechToText/Utils/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace RC_SpeechToText.Utils
+{
+    public class Converter
+    {
+        public string VideoToAudio()
+        {
+            var inputLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.mp4";
+            var outputLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.wav";
+
+            var inputFile = new MediaFile { Filename = inputLocation };
+            var outputFile = new MediaFile { Filename = outputLocation };
+
+            var conversionOptions = new ConversionOptions
+            {
+                AudioSampleRate = AudioSampleRate.Hz22050
+            };
+
+            try
+            {
+                using (var engine = new Engine())
+                {
+                    engine.GetMetadata(inputFile);
+
+                    engine.Convert(inputFile, outputFile, conversionOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Conversion Unsuccessful";
+            }
+            StereoToMono(outputLocation);
+            return "Conversion successful. Output file at: " + outputLocation;
+        }
+        public String StereoToMono(String outputLocation)
+        {
+            var outputLocationMono = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan_Mono.wav";
+            var outputWaveLocation = "C:\\Users\\sarb\\Desktop\\rad\\RAD_Vegan.wav";
+            try
+            {
+                using (var inputReader = new AudioFileReader(outputLocation))
+                {
+                    var mono = new StereoToMonoSampleProvider(inputReader);
+                    mono.LeftVolume = 0.0f;
+                    mono.RightVolume = 1.0f;
+                    WaveFileWriter.CreateWaveFile16(outputLocationMono, mono);
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Conversion Unsuccessful";
+            }
+            System.IO.File.Delete(outputWaveLocation);
+            return "Conversion from stereo to mono successful.";
+        }
+    }
+}
