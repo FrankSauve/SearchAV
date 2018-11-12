a703917f24935d63886a685937fa84f4af801a12
FrankSauve <franksauve@outlook.com>
2018-11-12 15:13:23 -0500

    Method to save the file to the audio directory
    Saves the file that was passed in http request

--- RC-SpeechToText/Controllers/TranscriptionController.cs
+++ RC-SpeechToText/Controllers/TranscriptionController.cs
@@ -21,23 +21,55 @@ namespace RC_SpeechToText.Controllers
         //[HttpPost("[action]")]
         //public GoogleTranscription ManageTranscription(IFormFile audioFile)
         //{
-            //once we get the file path (of the uploaded file) from the server, we use it to call the converter
-            string URL = null;
+        //once we get the file path (of the uploaded file) from the server, we use it to call the converter
+        // string URL = null;
 
-           // Utils.Converter converter;
-            //call converter manager to select if the file is a video or audio 
-          //  var audioFileConverted = converter.ConverterManager(URL); 
-           // Controllers.GoogleTranscription googleTranscription;
+        // Utils.Converter converter;
+        //call converter manager to select if the file is a video or audio 
+        //  var audioFileConverted = converter.ConverterManager(URL); 
+        // Controllers.GoogleTranscription googleTranscription;
 
 
-            //call the method that will get the transcription 
-           // var googleResponse = googleTranscription.GoogleSpeechToText2(audioFileConverted);
+        //call the method that will get the transcription 
+        // var googleResponse = googleTranscription.GoogleSpeechToText2(audioFileConverted);
 
-            //delete the converted file
+        //delete the converted file
 
-            //return the transcription
-          //  return googleResponse; 
-        
-       // }
+        //return the transcription
+        //  return googleResponse; 
+
+        // }
+
+
+        [HttpPost("[action]")]
+        public  GoogleTranscriptionResult GoogleSpeechToText2(IFormFile inputFile)
+        {
+            // Saves the file to the audio directory
+            var filePath = Directory.GetCurrentDirectory() + "\\Audio\\" + inputFile.FileName;
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                inputFile.CopyTo(stream);
+            }
+
+            var speech = SpeechClient.Create();
+            var response = speech.Recognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                LanguageCode = "fr-ca",
+                EnableWordTimeOffsets = true
+            }, RecognitionAudio.FromStream(audioFile.OpenReadStream())); // Add file name here
+
+            var googleTranscriptionResult = new GoogleTranscriptionResult
+            {
+                Transcription= response.Results[0]
+            };
+
+            return googleTranscriptionResult;
+        }
+    }
+
+    public class GoogleTranscriptionResult
+    {
+        public SpeechRecognitionResult Transcription { get; set; }
     }
 }
