7f1b4d44afb78127ab637794b18700087d582f9d
Sarb <sarbeng4@hotmail.com>
2018-11-13 10:12:43 -0500

    Added object-relational mapper (Entity Framework)
    Adding framework created Models folder with SearchAVContext.cs and Videos.cs

---
+++ RC-SpeechToText/Models/Videos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RC_SpeechToText.Models
+{
+    public partial class Videos
+    {
+        public int VideoId { get; set; }
+        public string Title { get; set; }
+        public string VideoPath { get; set; }
+        public string TranscriptionPath { get; set; }
+        public DateTime? DateAdded { get; set; }
+    }
+}
