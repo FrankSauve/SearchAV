7db432d3cfe0cde45faba4a7b24773d7284216a4
FrankSauve <franksauve@outlook.com>
2018-11-13 18:52:51 -0500

    Merge master and fix conflicts

--- CreateTaBleVideos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Videos (
-VideoID int IDENTITY(1,1) PRIMARY KEY,
-Title VARCHAR(256) NOT NULL,
-VideoPath VARCHAR(256),
-TranscriptionPath VARCHAR(256),
-DateAdded Date
-)
--- RC-SpeechToText/ClientApp/routes.tsx
+++ RC-SpeechToText/ClientApp/routes.tsx
@@ -4,6 +4,8 @@ import { Home } from './components/Home';
 import Navbar from './components/Navbar/Navbar';
 import TestAccuracy from './components/Google/TestAccuracy';
 import Transcription from './components/Google/Transcription'; 
+import { GetVideo } from './GetVideo';
+import { AddVideo } from './AddVideo';
 
 
 export const routes = <div>
@@ -11,4 +13,6 @@ export const routes = <div>
     <Route exact path='/' component={Home} />
     <Route exact path="/TestAccuracy" component={TestAccuracy} />
     <Route exact path="/Transcription" component={Transcription} />
+    <Route exact path="/videos" component={ GetVideo } />
+    <Route exact path="/videos" component={ AddVideo } />
 </div>;
