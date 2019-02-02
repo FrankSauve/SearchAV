730f9be1fd7f1f763a8e6fa7c6c430cd37de76c3
Francois Crispo-Sauve <franksauve@outlook.com>
2019-02-02 18:48:22 -0500

    Test for get all files with usernames

--- RC-SpeechToText.Tests/FileTest.cs
+++ RC-SpeechToText.Tests/FileTest.cs
@@ -9,35 +9,88 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace RC_SpeechToText.Tests
 {
     public class FileTest
     {
+        /// <summary>
+        /// Test fetching all the files
+        /// </summary>
         [Fact]
-        public async Task GettAllVideos()
+        public async Task TestGetAllVideos()
         {
+            // Arrange
             var options = new DbContextOptionsBuilder<SearchAVContext>().UseInMemoryDatabase().Options;
-
             var context = new SearchAVContext(options);
-            context.File.AddRange(Enumerable.Range(1, 20).Select(t => new File { Title = "Video " + t, FilePath = "vPath " + t }));
-            context.SaveChanges();
+
+            await context.File.AddRangeAsync(Enumerable.Range(1, 20).Select(t => new File { Title = "Video " + t, FilePath = "vPath " + t }));
+            await context.SaveChangesAsync();
 
             var mock = new Mock<ILogger<FileController>>();
             ILogger<FileController> logger = mock.Object;
-
-            //or use this short equivalent 
             logger = Mock.Of<ILogger<FileController>>();
 
             var controller = new FileController(context, logger);
 
             //Act
             var result = await controller.Index();
+
+            // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<List<File>>(okResult.Value);
             Assert.True(returnValue.Count() >= 0);
-            
+
         }
 
+        /// <summary>
+        /// Test fetching all the files with the corresponding usernames
+        /// </summary>
+        [Fact]
+        public async Task TestGetAllWithUsernames()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<SearchAVContext>().UseInMemoryDatabase().Options;
+            var context = new SearchAVContext(options);
+
+            // Add user with username testUser
+            var user = new User { Email = "test@email.com", Name = "testUser" };
+            await context.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            // Remove all file in DB
+            var files = await context.File.ToListAsync();
+            context.File.RemoveRange(files);
+            await context.SaveChangesAsync();
+
+            // Add file with title testFile
+            await context.File.AddAsync(new File { Title = "testFile", UserId = user.Id });
+            await context.SaveChangesAsync();
+
+            var mock = new Mock<ILogger<FileController>>();
+            ILogger<FileController> logger = mock.Object;
+            logger = Mock.Of<ILogger<FileController>>();
+
+            var controller = new FileController(context, logger);
+
+            // Act
+            var result = await controller.GetAllWithUsernames();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<JsonResult>(okResult.Value);
+
+            var json = JsonConvert.SerializeObject(okResult.Value);
+            dynamic data = JsonConvert.DeserializeObject<object>(json);
+
+            // Assert that username is testUser
+            string username = data.Value.usernames[0];
+            Assert.Equal("testUser", username);
+
+            // Assert that title is testFile
+            string fileTitle = data.Value.files[0].Title;
+            Assert.Equal("testFile", fileTitle);
+        }
     }
 }
