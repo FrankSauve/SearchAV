20e0362642dee61eb6b2cb49f27285875ed9b5cf
Francois Crispo-Sauve <franksauve@outlook.com>
2019-02-02 19:24:49 -0500

    Test for get version by file id

--- RC-SpeechToText.Tests/VersionTest.cs
+++ RC-SpeechToText.Tests/VersionTest.cs
@@ -15,6 +15,10 @@ namespace RC_SpeechToText.Tests
 {
     public class VersionTest
     {
+        /// <summary>
+        /// Test fetching all versions
+        /// Should return a list of versions
+        /// </summary>
         [Fact]
         public async Task TestGetAll()
         {
@@ -38,6 +42,41 @@ namespace RC_SpeechToText.Tests
             var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<List<Models.Version>>(okResult.Value);
             Assert.True(returnValue.Count() == 20);
+        }
+
+        /// <summary>
+        /// Test fetching one File by Id
+        /// Should return a list of versions
+        /// </summary>
+        [Fact]
+        public async Task TestGetByFileId()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<SearchAVContext>().UseInMemoryDatabase().Options;
+            var context = new SearchAVContext(options);
+
+            // Remove all versions from db
+            var versions = await context.Version.ToListAsync();
+            context.Version.RemoveRange(versions);
+            await context.SaveChangesAsync();
+
+            // Add file with title testFile
+            var version = new Models.Version { Transcription = "transcription", FileId = 1, UserId = 1 };
+            await context.Version.AddAsync(version);
+            await context.SaveChangesAsync();
+
+            var mock = new Mock<ILogger<FileController>>();
+            ILogger<FileController> logger = mock.Object;
+            logger = Mock.Of<ILogger<FileController>>();
+
+            var controller = new VersionController(context, logger);
+
+            // Act
+            var result = await controller.GetbyFileId(version.FileId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<Models.Version>>(okResult.Value);
         }
     }
 }
