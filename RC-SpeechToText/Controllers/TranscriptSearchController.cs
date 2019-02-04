0ce157f52545c2c1b651ef564199ae6ccbc012d5
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2019-02-04 11:18:48 -0500

    Backend search API done. - Now the search is done by fetching timestamps through database. - Method returns a list of timestamps where the word appears.

--- RC-SpeechToText/Controllers/TranscriptSearchController.cs
+++ RC-SpeechToText/Controllers/TranscriptSearchController.cs
@@ -57,7 +57,9 @@ namespace RC_SpeechToText.Controllers
             try
             {
                 _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Fetching all words for versionId: " + versionId);
-                var words = await _context.Word.Where(w => w.VersionId == versionId).ToListAsync();
+
+                var words = await _context.Word.Where(w => w.VersionId == versionId).OrderBy(w => w.Id).ToListAsync();
+                
                 return Ok(words);
             }
             catch (Exception ex)
@@ -65,73 +67,85 @@ namespace RC_SpeechToText.Controllers
                 _logger.LogError(ex, DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Error fetching all words for versionId: " + versionId);
                 return BadRequest("Error fetching active version with fileId: " + versionId);
             }
-            /*
-            _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Searching for " + searchTerms);
-            //Gets JSON as a string and then deserialize it into an object.
-            var fullResponse = JsonConvert.DeserializeObject<FullGoogleResponse>(jsonResponse);
-
-            //Check if the search terms are in the transcript
-            var timeStampOfTerms = new List<string>(); // Saves all instances of words timestamps
-            searchTerms = searchTerms.Trim();
-
-            string[] arrayTerms;
-
-            //Make sure the user did not pass an empty string
-            if (!String.IsNullOrEmpty(searchTerms))
-            {
-                arrayTerms = searchTerms.Split(' '); // Having an array of search terms to help when searching for timestamps  
-            }
-            else
-            {
-                return Ok("");
-            }
-
-            Words[] words = fullResponse.Words; // For clearer code instead of calling the full variable
-            _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Searching on words: " + fullResponse.Words);
-
-            //First check if serch terms are in the transcript, if they are look at where the word instances are located
-            if (fullResponse.Transcript.IndexOf(searchTerms, StringComparison.OrdinalIgnoreCase) >= 0)
-            {
-                //For each words check if it is what we were looking for.
-                for (var i = 0; i < words.Length; i++)
-                {
-                    //If first word of search term is equal to this current word, check if consecutive terms are equal.
-                    if (words[i].Word.Equals(arrayTerms[0], StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        for (var j = 0; j < arrayTerms.Length; j++)
-                        {
-
-                            //Make sure j doesn't go out of words range
-                            if (j < words.Length)
-                            {
-                                // If the next words in the sequence aren't the same: break
-                                if (!words[i + j].Word.Equals(arrayTerms[j], StringComparison.InvariantCultureIgnoreCase))
-                                {
-                                    break;
-                                }
-                                //If the last words of the serach terms we are looking for are equal, add this timestamp to our current list and increment i by j.
-                                else if (words[i + j].Word.Equals(arrayTerms[j], StringComparison.InvariantCultureIgnoreCase) && j == arrayTerms.Length - 1)
-                                {
-                                    //Adding the timestamp in the appropriate format
-                                    timeStampOfTerms.Add(TimeSpan.FromSeconds(words[i].StartTime.Seconds).ToString(@"hh\:mm\:ss"));
-                                    i = i + j;
-                                }
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-
-            //Getting all timestamps and converting them to string to make it easier when passing to frontend
-            var result = String.Join(", ", timeStampOfTerms.ToArray());
-            _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Time stamps of terms: " + timeStampOfTerms);
-
-            return Ok(result);*/
+           
         }
+
+        private string[] PerformSearch(string searchTerms, List<Models.Word> wordInfo) {
+            /*
+           _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Searching for " + searchTerms);
+           //Gets JSON as a string and then deserialize it into an object.
+           var fullResponse = JsonConvert.DeserializeObject<FullGoogleResponse>(jsonResponse);
+
+           //Check if the search terms are in the transcript
+           var timeStampOfTerms = new List<string>(); // Saves all instances of words timestamps
+           searchTerms = searchTerms.Trim();
+
+           string[] arrayTerms;
+
+           //Make sure the user did not pass an empty string
+           if (!String.IsNullOrEmpty(searchTerms))
+           {
+               arrayTerms = searchTerms.Split(' '); // Having an array of search terms to help when searching for timestamps  
+           }
+           else
+           {
+               return Ok("");
+           }
+
+           Words[] words = fullResponse.Words; // For clearer code instead of calling the full variable
+           _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Searching on words: " + fullResponse.Words);
+
+           //First check if serch terms are in the transcript, if they are look at where the word instances are located
+           if (fullResponse.Transcript.IndexOf(searchTerms, StringComparison.OrdinalIgnoreCase) >= 0)
+           {
+               //For each words check if it is what we were looking for.
+               for (var i = 0; i < words.Length; i++)
+               {
+                   //If first word of search term is equal to this current word, check if consecutive terms are equal.
+                   if (words[i].Word.Equals(arrayTerms[0], StringComparison.InvariantCultureIgnoreCase))
+                   {
+                       for (var j = 0; j < arrayTerms.Length; j++)
+                       {
+
+                           //Make sure j doesn't go out of words range
+                           if (j < words.Length)
+                           {
+                               // If the next words in the sequence aren't the same: break
+                               if (!words[i + j].Word.Equals(arrayTerms[j], StringComparison.InvariantCultureIgnoreCase))
+                               {
+                                   break;
+                               }
+                               //If the last words of the search terms we are looking for are equal, add this timestamp to our current list and increment i by j.
+                               else if (words[i + j].Word.Equals(arrayTerms[j], StringComparison.InvariantCultureIgnoreCase) && j == arrayTerms.Length - 1)
+                               {
+                                   //Adding the timestamp in the appropriate format
+                                   timeStampOfTerms.Add(TimeSpan.FromSeconds(words[i].StartTime.Seconds).ToString(@"hh\:mm\:ss"));
+                                   i = i + j;
+                               }
+                           }
+                           else
+                           {
+                               break;
+                           }
+                       }
+                   }
+               }
+           }
+
+           //Getting all timestamps and converting them to string to make it easier when passing to frontend
+           var result = String.Join(", ", timeStampOfTerms.ToArray());
+           _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n\t Time stamps of terms: " + timeStampOfTerms);
+           */
+            string[] result = { "x"};
+           return result;
+        }
+
+        /*private Models.Words[] stringToWordList(List<String> wordInfo)
+        {
+            Models.Words[] allWords;
+
+            return x;
+        }*/
 
     }
 }
