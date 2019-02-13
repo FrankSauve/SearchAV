45951763c76336ff65534226f9f6ab3d95887ed4
williamkingbede <williamkingbede@gmail.com>
2019-02-13 11:44:21 -0500

    Added modal showing users registered in system when button clicked #128

---
+++ RC-SpeechToText/ClientApp/components/Modals/SelectReviewerModal.tsx
@@ -0,0 +1,79 @@
+﻿import * as React from 'react';
+import axios from 'axios';
+import auth from '../../Utils/auth';
+
+interface State {
+    users: any[],
+    unauthorized: boolean
+}
+
+
+export class SelectReviewerModal extends React.Component<any, State> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            users: [],
+            unauthorized: false
+        }
+    }
+
+    // Called when the component gets rendered
+    public componentDidMount() {
+        this.getAllUsers();
+    }
+
+    public getAllUsers = () => {
+
+        const config = {
+            headers: {
+                'Authorization': 'Bearer ' + auth.getAuthToken(),
+                'content-type': 'application/json'
+            }
+        }
+        axios.get('/api/user/GetAllUsers', config)
+            .then(res => {
+                console.log(res.data);
+                this.setState({ 'users': res.data });
+            })
+            .catch(err => {
+                console.log(err);
+                if (err.response.status == 401) {
+                    this.setState({ 'unauthorized': true });
+                }
+            });
+    };
+
+    public render() {
+        return (
+            <div className={`modal ${this.props.showModal ? "is-active" : null}`} >
+                <div className="modal-background"></div>
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">Choisissez un reviseur</p>
+                        <button className="delete" aria-label="close" onClick={this.props.hideModal}></button>
+                    </header>
+                    <section className="modal-card-body">
+                        <div className="select is-multiple">
+                            <select multiple size={8}>
+                                {this.state.users.map((user) => {
+                                    {
+                                        //Includes current user's name for testing purposes
+                                    }
+                                    const listUsers = <option value={user.id}>{user.name}</option>
+                                    return (
+                                        listUsers
+                                    )
+                                })}
+                            </select>
+                        </div>
+                    </section>
+                    <footer className="modal-card-foot">
+                        <button className="button is-success">Envoyer la demande</button>
+                        <button className="button" onClick={this.props.hideModal}>Annuler</button>
+                    </footer>
+                </div>
+            </div>
+        );
+    }
+}
--- RC-SpeechToText/ClientApp/components/Navbar/Navbar.tsx
+++ RC-SpeechToText/ClientApp/components/Navbar/Navbar.tsx
@@ -3,13 +3,29 @@ import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import Login from './Login';
 import Logout from './Logout';
+import { SelectReviewerModal } from '../Modals/SelectReviewerModal';
 import auth from '../../Utils/auth'
 
 interface State {
-
+    showModal: boolean
 }
 
 export default class Navbar extends React.Component<RouteComponentProps<{}>, State> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            showModal: false
+        }
+    }
+
+    public showModal = () => {
+        this.setState({ showModal: true });
+    }
+
+    public hideModal = () => {
+        this.setState({ showModal: false });
+    }
 
     // Shown when the user is logged in
     public renderLoggedIn = () => {
@@ -42,7 +58,7 @@ export default class Navbar extends React.Component<RouteComponentProps<{}>, Sta
                     <Link className="navbar-item" to="/dashboard">
                         <i className="fas fa-angle-left"></i>
                     </Link>
-                    <a className="button is-rounded mg-top-10 mg-left-100"><i className="far fa-envelope"></i> Demander une revision</a>
+                    <a className="button is-rounded mg-top-10 mg-left-400" onClick={this.showModal}><i className="far fa-envelope"></i> Demander une revision</a>
                     <a className="button is-rounded mg-top-10 mg-left-10"><i className="fas fa-file-export"></i> Exporter</a>
                 <a className="button is-rounded is-link mg-top-10 mg-left-10"><i className="far fa-save"></i> Enregistrer</a>
                 </div>
@@ -73,6 +89,11 @@ export default class Navbar extends React.Component<RouteComponentProps<{}>, Sta
                         </a>
                         
                         {this.isInFileViewPage() ? this.renderFileViewNav() : this.renderNormalNav()}
+
+                        <SelectReviewerModal
+                            showModal={this.state.showModal}
+                            hideModal={this.hideModal}
+                        />
                         
 
                             <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
