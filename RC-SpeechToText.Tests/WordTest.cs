f3e4ccb12397d700ab8eef69b86a1e1787fa4dfa
Francois Crispo-Sauve <franksauve@outlook.com>
2019-02-15 15:49:47 -0500

    Added get words by version Id with unit test #73

---
+++ RC-SpeechToText.Tests/WordTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RC_SpeechToText.Controllers;
+using RC_SpeechToText.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RC_SpeechToText.Tests
+{
+    public class WordTest
+    {
+        [Fact]
+        public async Task TestGetByVersionId()
+        {
+            // Arrange
+            var context = new SearchAVContext(DbContext.CreateNewContextOptions());
+
+            // Add 20 words to DB
+            await context.Word.AddRangeAsync(Enumerable.Range(1, 20).Select(t => new Word { Term = "test", VersionId = 1 }));
+            await context.SaveChangesAsync();
+
+            var mock = new Mock<ILogger<FileController>>();
+            ILogger<FileController> logger = mock.Object;
+            logger = Mock.Of<ILogger<FileController>>();
+
+            // Act
+            var controller = new WordController(context, logger);
+            var result = await controller.GetByVersionId(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<Word>>(okResult.Value);
+            Assert.Equal(20, returnValue.Count());
+        }
+    }
+}
--- RC-SpeechToText/Controllers/WordController.cs
+++ RC-SpeechToText/Controllers/WordController.cs
@@ -25,6 +25,32 @@ namespace RC_SpeechToText.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get all words with versionId
+        /// </summary>
+        /// <param name="versionId"></param>
+        /// <returns></returns>
+        [HttpGet("[action]/{versionId}")]
+        public async Task<IActionResult> GetByVersionId(int versionId)
+        {
+            try
+            {
+                _logger.LogInformation(DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n Getting all words for version with id: " + versionId);
+                var words = await _context.Word.Where(w => w.VersionId == versionId).ToListAsync();
+                return Ok(words);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, DateTime.Now.ToString(_dateConfig) + " - " + this.GetType().Name + " \n Error getting all words for version with id: " + versionId);
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Delete all words with fileId
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
         [HttpDelete("[action]/{fileId}")]
         public async Task<IActionResult> DeleteByFileId(int fileId)
         {
