b2f329fbb434b6ca1c33b26fc39810234f497047
Sarb <sarbeng4@hotmail.com>
2019-02-19 22:08:01 -0500

    Duplicate from previous commit #109

--- RC-SpeechToText/Scripts/test.sql
+++ RC-SpeechToText/Scripts/test.sql
@@ -1,8 +1,12 @@
 ﻿/* alter table [File] add [Description] VARCHAR (MAX) NULL
 update  [file] set description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis urna, posuere vel augue a, sollicitudin sagittis nulla. Curabitur tellus nulla, feugiat nec nisl sed, condimentum feugiat enim. Nullam iaculis'
-select * from [file]*/
+select * from [file]
+select * from [user]*/
 
-select * from [user]
+alter table [User] add [Job_Tile] VARCHAR (MAX) NULL
+update  [User] set Job_Tile = 'Editor' where ID = 103
+SELECT * FROM [User]
 
-show [tables]
+
+
 
