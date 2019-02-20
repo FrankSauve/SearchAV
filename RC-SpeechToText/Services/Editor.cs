831709b56c28d34adaf8e71f34f20bcf91fa8b69
Sarb <sarbeng4@hotmail.com>
2019-02-19 22:57:08 -0500

    Added "Editor" concrete observer class #102

---
+++ RC-SpeechToText/Services/Editor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RC_SpeechToText.Services
+{
+    /// <summary>
+    /// The 'Concrete Observer' class
+    /// </summary>
+    public class Editor : IEmailNotification
+    {
+        private string _name;
+        private string _email;
+
+        // Constructor
+        public Editor(string name, string email)
+        {
+            this._name = name;
+            this._email = email;
+        }
+
+        public void Update()
+        {
+        }
+
+    }
+}
--- RC-SpeechToText/Services/IEmailNotification.cs
+++ RC-SpeechToText/Services/IEmailNotification.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RC-SpeechToText.Services
+namespace RC_SpeechToText.Services
 {
+    /// <summary>
+    /// The 'Observer' interface
+    /// </summary>
     interface IEmailNotification
-{
-}
+    {
+        void Update();
+    }
 }
