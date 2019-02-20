4084e700696d7ca7dd2e7046f2f9e5358eb4b60a
Sarb <sarbeng4@hotmail.com>
2019-02-19 22:04:47 -0500

    Added Job_Tiltle to user table #102
    This will allow title to be displayed alongside user name.
    This will also allow notification to be done properly to correct role.

--- RC-SpeechToText/Scripts/test.sql
+++ RC-SpeechToText/Scripts/test.sql
@@ -1,3 +1,8 @@
-﻿/* alter table [File] add [Description] VARCHAR (MAX) NULL*/
+﻿/* alter table [File] add [Description] VARCHAR (MAX) NULL
 update  [file] set description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis urna, posuere vel augue a, sollicitudin sagittis nulla. Curabitur tellus nulla, feugiat nec nisl sed, condimentum feugiat enim. Nullam iaculis'
-select * from [file]
+select * from [file]*/
+
+select * from [user]
+
+show [tables]
+
