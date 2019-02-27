222bd40e72b135c077d0d0314d257efb3a0aeee1
PhilippeKuret <philippekuret@gmail.com>
2019-02-26 22:40:20 -0500

    Create Word document service

---
+++ RC-SpeechToText/Services/ExportTranscriptionService.cs
@@ -0,0 +1,22 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+
+namespace RC_SpeechToText.Services
+{
+    public class ExportTranscriptionService
+	{
+		public void CreateWordDocument(string transcription)
+		{
+			Word.Application app = new Word.Application();
+			Word.Document doc = app.Documents.Add();
+
+			object start = 0;
+			object end = transcription.Length;
+
+			Word.Range range = doc.Range(ref start, ref end);
+			range.Text = transcription;
+			range.Select();
+
+			doc.Save();
+		}
+	}
+}
