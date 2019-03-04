ddb921538ec0b4560e9a673998c7992c33387850
williamkingbede <williamkingbede@gmail.com>
2019-03-04 15:12:35 -0500

    Refactored style of basic success, confirmation and error modals

--- RC-SpeechToText/ClientApp/components/Modals/SuccessModal.tsx
+++ RC-SpeechToText/ClientApp/components/Modals/SuccessModal.tsx
@@ -11,16 +11,18 @@ export class SuccessModal extends React.Component<any> {
 
             <div className={`modal ${this.props.showModal ? "is-active" : null}`} >
                 <div className="modal-background"></div>
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                        <p className="modal-card-title">{this.props.title}</p>
-                        <button className="delete" aria-label="close" onClick={this.props.hideModal}></button>
-                    </header>
-                    <section className="modal-card-body">
-                        <p className="has-text-success">{this.props.successMessage}</p>
-                    </section>
-                    <footer className="modal-card-foot">
-                    </footer>
+                <div className="modal-card modalCard">
+                    <div className="modal-container">
+                        <header className="modalHeader">
+                            <i className="fas fa-check-circle mg-right-10"></i><p className="modal-card-title whiteText">{this.props.title}</p>
+                            <button className="delete closeModal" aria-label="close" onClick={this.props.hideModal}></button>
+                        </header>
+                        <section className="modalBody">
+                            <div className="modalSection padding-bottom-10">
+                                <p>{this.props.successMessage}</p>
+                            </div>
+                        </section>
+                    </div>
                 </div>
             </div>
         );
