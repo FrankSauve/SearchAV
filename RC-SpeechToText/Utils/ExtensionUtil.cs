722aae56ee7f5c6a07c2e61c8fe5533dc6aa76ea
PhilippeKuret <philippekuret@gmail.com>
2019-03-06 11:58:30 -0500

    Add Extension util with string extension isNullOrEmpty

---
+++ RC-SpeechToText/Utils/ExtensionUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RC_SpeechToText.Utils
+{
+	public static class ExtentionUtil
+	{
+		public static bool IsNullOrEmpty(this string str)
+		{
+			if (str == null || str.Trim() == "" || str.Length == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
