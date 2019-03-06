901713f6ea3910a513ced1c299f1688b5dc5a14f
PhilippeKuret <philippekuret@gmail.com>
2019-03-05 23:35:35 -0500

    Credential google doc

--- RC-SpeechToText/Services/ExportTranscriptionService.cs
+++ RC-SpeechToText/Services/ExportTranscriptionService.cs
@@ -1,4 +1,14 @@
-﻿using Word = Microsoft.Office.Interop.Word;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Docs.v1;
+using Google.Apis.Docs.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using static Google.Apis.Docs.v1.DocsService;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace RC_SpeechToText.Services
 {
@@ -25,6 +35,41 @@ namespace RC_SpeechToText.Services
 			{
 				return false;
 			}
+		}
+
+		public bool CreateGoogleDocument(string transcription)
+		{
+			try
+			{
+				var credential = GetUserCredential();
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		private UserCredential GetUserCredential()
+		{
+			UserCredential credential;
+			string[] Scopes = { DocsService.Scope.Documents };
+
+			using (var stream =
+				new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+			{
+				string credPath = "token.json";
+
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+			}
+
+			return credential;
 		}
 	}
 }
