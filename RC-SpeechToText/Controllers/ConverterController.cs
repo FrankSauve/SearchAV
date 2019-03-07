da10ca2efe3a9dc255a4a0cc5dc6d75d800b9ebf
Sarb <sarbeng4@hotmail.com>
2019-03-07 03:22:45 -0500

    Thumbnail Fix #94
    When thumbnails are added automatically, path to thumbnail is stored in the DB.
    When file title is modified, so is name of thumbnail and path to thumbnail is updated.

--- RC-SpeechToText/Controllers/ConverterController.cs
+++ RC-SpeechToText/Controllers/ConverterController.cs
@@ -124,8 +124,9 @@ namespace RC_SpeechToText.Controllers
 				UserId = user.Id,
 				DateAdded = DateTime.Now,
                 Type = fileType,
-				//Description = "" 
-			};
+                ThumbnailPath = @"\wwwroot\assets\Thumbnails\" + ".jpg",
+                //Description = "" 
+            };
 			await _context.File.AddAsync(file);
 			await _context.SaveChangesAsync();
 
