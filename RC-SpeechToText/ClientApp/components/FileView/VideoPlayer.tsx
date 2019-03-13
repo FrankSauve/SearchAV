f8908c460a20015e1c1ef9b5ffdf3c8c289b02b2
Francois Crispo-Sauve <franksauve@outlook.com>
2019-03-13 15:00:11 -0400

    Change the video player library and we can now seek at right time #73
    Co-Authored-By: William Leclerc <lecleric@users.noreply.github.com>

--- RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
+++ RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
@@ -1,21 +1,38 @@
 import * as React from 'react';
-import {Player, ControlBar} from 'video-react';
+import ReactPlayer from 'react-player';
 
-export class VideoPlayer extends React.Component<any> {
+interface State {
+    player: any
+}
+
+export class VideoPlayer extends React.Component<any, State> {
     constructor(props: any) {
         super(props);
+
+        this.state = {
+            player: null
+        }
     }
 
+    public changeTime = () => {
+        this.state.player.seekTo(11);
+    }
+    
+    ref = (player: any) => {
+        this.setState({player: player});
+      }
     public render() {
         return (
             <div>
-                <Player
-                    ref="player"
-                    autoplay
-                >
-                    <source src={'../assets/Audio/' + this.props.path} />
-                    <ControlBar autoHide={false}/>
-                </Player>
+                <ReactPlayer
+                    ref={this.ref}
+                    url={'../assets/Audio/' + this.props.path}
+                    playing={false}
+                    controls={true}
+                    width='100%'
+                    height='100%'
+                />
+                <button onClick={this.changeTime}>Change time yo</button>
             </div>
         );
     }
