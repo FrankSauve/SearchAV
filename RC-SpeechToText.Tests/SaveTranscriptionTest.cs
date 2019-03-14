d63ea2e1f7d8fd68ec41a17c26badce25e4a0a6c
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2019-03-13 23:31:22 -0400

    Fixed tests related to transcript controller. - Added email claims in test methods related to saveTranscript, using security claims.

--- RC-SpeechToText.Tests/SaveTranscriptionTest.cs
+++ RC-SpeechToText.Tests/SaveTranscriptionTest.cs
@@ -10,6 +10,8 @@ using Version = RC_SpeechToText.Models.Version;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace RC_SpeechToText.Tests
 {
@@ -28,6 +30,14 @@ namespace RC_SpeechToText.Tests
             var reviewer = new User {Id = 2, Email = "reviewer@email.com", Name = "testReviewer" };
             var file = new File { Title = "title", DateAdded = DateTime.Now, Flag = "Automatisé", UserId = user.Id, ReviewerId = reviewer.Id };
 
+            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", user.Email)
+            }));
+            var reviewerPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", reviewer.Email)
+            }));
             //AddAsync File to database
             await context.File.AddAsync(file);
             await context.SaveChangesAsync();
@@ -43,9 +53,12 @@ namespace RC_SpeechToText.Tests
             string reviewTranscription = "Test Review Transcription";
 
             var controller = new TranscriptionController(context);
-
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = userPrincipal }
+            };
             //Editing file
-            await controller.SaveTranscript(user.Id, version.Id, editTranscription);
+            await controller.SaveTranscript( version.Id, editTranscription);
             Assert.NotEqual(version.Transcription, editTranscription);
 
             //Checking edited version
@@ -60,7 +73,11 @@ namespace RC_SpeechToText.Tests
 
             //----------------------------------------------------------------------------------------------------------------
             //Review file
-            await controller.SaveTranscript(reviewer.Id, editedVersion.Id, reviewTranscription);
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = reviewerPrincipal }
+            };
+            await controller.SaveTranscript(editedVersion.Id, reviewTranscription);
             Assert.NotEqual(editedVersion.Transcription, reviewTranscription);
 
             //Checking new version
@@ -96,6 +113,11 @@ namespace RC_SpeechToText.Tests
             var reviewer = new User { Id = 2, Email = "reviewer@email.com", Name = "testReviewer" };
             var file = new File { Title = "title", DateAdded = DateTime.Now, Flag = "Automatisé", UserId = user.Id, ReviewerId = reviewer.Id };
 
+            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", user.Email)
+            }));
+
             //AddAsync File to database
             await context.File.AddAsync(file);
             await context.SaveChangesAsync();
@@ -127,9 +149,12 @@ namespace RC_SpeechToText.Tests
             string addWordsTranscription = "Un Deux DeuxDeux Trois Quatre Cinq Six Sept Huit";// Added DeuxDeux and Huit
             
             var controller = new TranscriptionController(context);
-
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = userPrincipal }
+            };
             //Editing file to add Words
-            await controller.SaveTranscript(user.Id, version.Id, addWordsTranscription);
+            await controller.SaveTranscript( version.Id, addWordsTranscription);
 
             //Get the words for new version
             List<Word> addedWords = await context.Word.Where(w => w.VersionId == version.Id + 1).OrderBy(w => w.Id).ToListAsync();
@@ -159,6 +184,11 @@ namespace RC_SpeechToText.Tests
             var user = new User { Id = 1, Email = "user@email.com", Name = "testUser" };
             var reviewer = new User { Id = 2, Email = "reviewer@email.com", Name = "testReviewer" };
             var file = new File { Title = "title", DateAdded = DateTime.Now, Flag = "Automatisé", UserId = user.Id, ReviewerId = reviewer.Id };
+
+            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("email", user.Email)
+            }));
 
             //AddAsync File to database
             await context.File.AddAsync(file);
@@ -192,8 +222,12 @@ namespace RC_SpeechToText.Tests
 
             var controller = new TranscriptionController(context);
 
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = userPrincipal }
+            };
             //Editing file to delete words
-            await controller.SaveTranscript(user.Id, version.Id, addWordsTranscription);
+            await controller.SaveTranscript( version.Id, addWordsTranscription);
 
             //Get the words for new version
             List<Word> addedWords = await context.Word.Where(w => w.VersionId == version.Id + 1).OrderBy(w => w.Id).ToListAsync();
@@ -217,6 +251,11 @@ namespace RC_SpeechToText.Tests
             var user = new User { Id = 1, Email = "user@email.com", Name = "testUser" };
             var reviewer = new User { Id = 2, Email = "reviewer@email.com", Name = "testReviewer" };
             var file = new File { Title = "title", DateAdded = DateTime.Now, Flag = "Automatisé", UserId = user.Id, ReviewerId = reviewer.Id };
+
+            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+           {
+                new Claim("email", user.Email)
+           }));
 
             //AddAsync File to database
             await context.File.AddAsync(file);
@@ -249,9 +288,12 @@ namespace RC_SpeechToText.Tests
             string addWordsTranscription = "Uno Deux Trois Quatre Cinqo Six Seven";// Modified word Un/Cinq/Sept
 
             var controller = new TranscriptionController(context);
-
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = userPrincipal }
+            };
             //Editing file to modify words
-            await controller.SaveTranscript(user.Id, version.Id, addWordsTranscription);
+            await controller.SaveTranscript( version.Id, addWordsTranscription);
 
             //Get the words for new version
             List<Word> addedWords = await context.Word.Where(w => w.VersionId == version.Id + 1).OrderBy(w => w.Id).ToListAsync();
