3f8413b624d650c3571d8e1006ab91331e8d02ba
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2019-03-14 14:41:18 -0400

    Merge Conflicts With Master branch.

--- RC-SpeechToText/Models/File.cs
+++ RC-SpeechToText/Models/File.cs
@@ -16,8 +16,6 @@ namespace RC_SpeechToText.Models
         public User Reviewer { get; set; }
         public string Description { get; set; }
         public string ThumbnailPath { get; set; }
-
-		public User User { get; set; }
-		public User Reviewer { get; set; }
+        
 	}
 }
