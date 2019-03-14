531eb8222245d846ac5c8c19efc0ab12da154714
LeCleric <13441191+LeCleric@users.noreply.github.com>
2019-03-14 19:44:31 -0400

    Added keyboard shortcuts for videos #203
    Controls:
    Ctrl+← = seek 5 seconds before current time
    Ctrl+↓ || Ctrl+Space = pause/play video
    Ctrl+→ = seek 5 seconds after current time

--- RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
+++ RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import ReactPlayer from 'react-player';
 
 interface State {
-    player: any
+    player: any,
+    playing: boolean,
+    
 }
 
 export class VideoPlayer extends React.Component<any, State> {
@@ -10,10 +12,57 @@ export class VideoPlayer extends React.Component<any, State> {
         super(props);
 
         this.state = {
-            player: null
+            player: null,
+            playing: false
         }
+
     }
 
+    handleKeyDown = (event: any) => {
+        //console.log("isPlaying? " + this.state.isPlaying);
+        
+        if(event.ctrlKey) { //ctrl key
+            if (event.keyCode == 37) { //left arrow
+                this.localSeek(-5);
+            }
+            else if (event.keyCode == 40 || event.keyCode == 32) { //down arrow or space
+                this.playPause();
+            }
+            else if (event.keyCode == 39) { //right arrow
+                this.localSeek(5);
+            }
+        }
+    };
+    
+    localSeek = (n: any) =>{
+        let seekVal = this.state.player.getCurrentTime()+n;
+        if (seekVal < 0)
+            seekVal = 0;
+        if(seekVal >= this.state.player.getDuration())
+            seekVal = this.state.player.getDuration() - 1;
+        this.state.player.seekTo(seekVal);
+    };
+    
+    playPause = () =>{
+        this.setState({playing: !this.state.playing})
+    };
+    
+    onPlay = () =>{
+        this.setState({playing:true})
+    };
+    onPause = () =>{
+        this.setState({playing:false})
+    };
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    
     componentDidUpdate(prevProps : any, prevState : any) {
         // only call for the change in time if the data has changed
         if (prevProps.seekTime !== this.props.seekTime) {
@@ -22,22 +71,24 @@ export class VideoPlayer extends React.Component<any, State> {
     }
 
     public changeTime = () => {
-        var hrs = parseInt(this.props.seekTime.substring(0, 1));
-        var mins = parseInt(this.props.seekTime.substring(2, 2) + hrs * 60.0);
-        var secs = parseFloat(this.props.seekTime.substring(5) + mins * 60.0);
+        let hrs = parseInt(this.props.seekTime.substring(0, 1));
+        let mins = parseInt(this.props.seekTime.substring(2, 2) + hrs * 60.0);
+        let secs = parseFloat(this.props.seekTime.substring(5) + mins * 60.0);
         this.state.player.seekTo(secs);
-    }
+    };
     
     ref = (player: any) => {
         this.setState({player: player});
-      }
+    };
     public render() {
         return (
             <div>
                 <ReactPlayer
                     ref={this.ref}
                     url={'../assets/Audio/' + this.props.path}
-                    playing={false}
+                    playing={this.state.playing}
+                    onPause={this.onPause}
+                    onPlay={this.onPlay}
                     controls={true}
                     width='100%'
                     height='100%'
