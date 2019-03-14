e4eb2ceacee6d5b4d7a980252f01c6d2730096c0
LeCleric <13441191+LeCleric@users.noreply.github.com>
2019-03-13 20:43:46 -0400

    Made highlight seeking automatic and removed placeholder button #73

--- RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
+++ RC-SpeechToText/ClientApp/components/FileView/VideoPlayer.tsx
@@ -14,11 +14,17 @@ export class VideoPlayer extends React.Component<any, State> {
         }
     }
 
+    componentDidUpdate(prevProps : any, prevState : any) {
+        // only call for the change in time if the data has changed
+        if (prevProps.seekTime !== this.props.seekTime) {
+            this.changeTime();
+        }
+    }
+
     public changeTime = () => {
         var hrs = parseInt(this.props.seekTime.substring(0, 1));
-        var mins = parseInt(this.props.seekTime.substring(2, 2) + hrs*60);
-        var secs = parseFloat(this.props.seekTime.substring(5) + mins*60);
-        console.log("seekTime in hours: "+ hrs + ", mins: " + mins + ", secs:" + secs);
+        var mins = parseInt(this.props.seekTime.substring(2, 2) + hrs * 60.0);
+        var secs = parseFloat(this.props.seekTime.substring(5) + mins * 60.0);
         this.state.player.seekTo(secs);
     }
     
@@ -36,7 +42,6 @@ export class VideoPlayer extends React.Component<any, State> {
                     width='100%'
                     height='100%'
                 />
-                <button onClick={this.changeTime}>Change time yo</button>
             </div>
         );
     }
