4bbe8fd8950eca43df2a6d55c367691f6d25fe53
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2019-03-16 01:52:20 -0400

    Added Custom Controller Exception and More Intricate Exception logs - Added a custom exception class for our controllers exceptions. - Added a controller error object to pass it to client. - More intricate logging when exception is thrown.

---
+++ RC-SpeechToText/Exceptions/ControllerExceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RC_SpeechToText.Exceptions
+{
+    public class ControllerExceptions : Exception
+{
+        public int StatusCode { get; set; }
+
+        public ControllerExceptions(string message) : base(message)
+        {
+            StatusCode = 400;
+        }
+        
+    }
+
+    //To generate JSON that will be passed to client.
+    public class ControllerError
+    {
+        public string message { get; set; }
+        public bool isError { get; set; }
+        public string detail { get; set; }
+
+        public ControllerError(string message)
+        {
+            this.message = message;
+            isError = true;
+        }
+        public ControllerError(string message, string detail)
+        {
+            this.message = message;
+            this.detail = detail;
+            isError = true;
+        }
+
+    }
+
+}
+
--- RC-SpeechToText/Filters/ControllerExceptionFilter.cs
+++ RC-SpeechToText/Filters/ControllerExceptionFilter.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Logging;
+using RC_SpeechToText.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +22,49 @@ namespace RC_SpeechToText.Filters
 
         public override void OnException(ExceptionContext context)
         {
-            _logger.LogError("Exception thrown");
+            //Retrieve User IP
+            var ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            //Retrieve Route that was called
+            var urlHelper = new UrlHelper(context);
+            var route = urlHelper.RouteUrl(context.RouteData.Values);
+
+            //Get arguments that caused the exception
+            var arguments = context.ActionDescriptor.RouteValues;
+            var argumentsString = string.Join("; ", arguments.Select(x => x.Key + "=" + x.Value).ToArray());
+
+            ControllerError controllerError = null;
+            //Handling known Controller errors
+            if (context.Exception is ControllerExceptions)
+            {
+                // Handling controller errors 
+                var ex = context.Exception as ControllerExceptions;
+                context.Exception = null;
+                controllerError = new ControllerError(ex.Message, ex.StackTrace);
+
+                context.HttpContext.Response.StatusCode = ex.StatusCode;
+
+            }
+            else
+            {
+                // Handling unexpected errors
+                var msg = context.Exception.GetBaseException().Message;
+                string stack = context.Exception.StackTrace;
+
+                controllerError = new ControllerError(msg, stack);
+                context.HttpContext.Response.StatusCode = 500;
+            }
+
+            _logger.LogError("Action Executing with [" +
+                "\nIP: " + ip +
+                "\nRoute Called: " + route +
+                "\nArguments: " + argumentsString +
+                "\nException: " + controllerError.message +
+                "\nTrace: " + controllerError.detail + "]"
+                );
+
+            context.Result = new JsonResult(controllerError);
+
             base.OnException(context);
         }
     }
