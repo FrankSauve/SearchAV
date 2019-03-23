e33c16a306be25607633805a6f34fde7c48e8038
LeCleric <13441191+LeCleric@users.noreply.github.com>
2019-03-23 14:31:50 -0400

    changed highlight colour #55

--- RC-SpeechToText/ClientApp/components/FileView/TranscriptionText.tsx
+++ RC-SpeechToText/ClientApp/components/FileView/TranscriptionText.tsx
@@ -73,7 +73,7 @@ export class TranscriptionText extends React.Component<any, State> {
             for( let i=0 ; i<textArray.length; i++ ){
                 hTextArray.push(textArray[i]);
                 if(i!=textArray.length-1){
-                    hTextArray.push("<span style='background-color: lightblue'>");
+                    hTextArray.push("<span style='background-color: #b9e0f9'>");
                     hTextArray.push(s);
                     hTextArray.push("</span>");
                 }
