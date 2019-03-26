eab323722b0ac7899a5384dd6ba41d9515684ac4
williamkingbede <williamkingbede@gmail.com>
2019-03-26 13:46:30 -0400

    File view file info style fixed #228

--- RC-SpeechToText/ClientApp/components/FileView/FileView.tsx
+++ RC-SpeechToText/ClientApp/components/FileView/FileView.tsx
@@ -270,36 +270,31 @@ export default class FileView extends React.Component<any, State> {
                         {/* Using title for now, this will have to be change to path eventually */}
                         {this.state.file ? <VideoPlayer path={this.state.file.title} seekTime={this.state.seekTime} /> : null}
 
-                        {this.state.file ? <b className="has-text-link">Titre: </b> : null}
-                        {this.state.file ? (this.state.file.title ? <div><div className="card">
-                            <div className="card-content">
+                        {this.state.file ? <b className="file-view-header">Titre: </b> : null}
+                        {this.state.file ? (this.state.file.title ? <div>
+                            <div className="file-view-title">
                                 {this.state.file.title}
-                            </div> </div></div> : <div className="card">
-                                <div className="card-content"> This file has no title </div></div>) : null}
+                            </div> </div> : <div className="file-view-title"> This file has no title </div>) : null}
                         
                         <br />
 
-                        {this.state.file ? <b className="has-text-link">Description: <a onClick={this.showDescriptionModal}><i className="fas fa-edit"></i></a></b> : null}
+                        {this.state.file ? <b className="file-view-header">Description: <a onClick={this.showDescriptionModal}><i className="fas fa-edit"></i></a></b> : null}
                         {this.state.file ? (this.state.file.description ? <div>
-                            <div className="card">
-                                <div className="card-content">
+                            <div className="file-view-desc">
                                 {this.state.description}
-                                </div>
+                            </div>
 
-                            </div></div> : <div className="card">
-                                <div className="card-content"> This file has no description </div></div>) : null}
+                        </div> : <div className="file-view-desc"> This file has no description </div>) : null}
                         
                         <br />
 
-                        <p>{this.state.file ? (this.state.file ? <div><div className="card">
-                            <div className="card-content">
-                                {this.state.version ?
+                        <p>{this.state.file ? (this.state.version ?
                                     <FileInfo
                                         thumbnail={this.state.file.thumbnailPath}
                                         userId={this.state.file.userId}
-                                        dateModified={this.state.version.dateModified} /> : null}
-                            </div> </div></div> : <div className="card">
-                                <div className="card-content"> This file has no extra Info </div></div>) : null}</p>
+                                        dateModified={this.state.version.dateModified}
+                                    /> : <p>This file has no extra Info </p>)
+                            : null}</p>
                     </div>
 
                     <div className="column is-half mg-top-30">
