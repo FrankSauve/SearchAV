b83b578717fd87e722ba54a60287ce6d8e216621
AyoubeAkaouch <ayoube.akaouch@gmail.com>
2019-03-28 01:04:21 -0400

    Removed the old methods for saving a transcription

--- RC-SpeechToText/Services/ModifyTimeStampService.cs
+++ RC-SpeechToText/Services/ModifyTimeStampService.cs
@@ -16,24 +16,7 @@ namespace RC_SpeechToText.Services
                 longestCommonSequence.newTransPosition, longestCommonSequence.oldTransPositions, newVersionId);
 
             newWords = EstimateWords(newWords, duration);
-            //Old logic to be deleted after implementation of new save algorithm
-            /*List<Word> newWords = new List<Word>();
-
-            var newTranscriptNoBr = longestCommonSequence.newTranscriptionTerms;
-
-
-            if (newTranscriptNoBr.Count == oldWords.Count)
-            {
-                newWords = HandleEdited(oldWords, newTranscript, newVersionId, newTranscriptNoBr);
-            }
-            else if (newTranscriptNoBr.Count < oldWords.Count)
-            {
-                newWords = HandleDeleted(oldWords, newTranscript, newVersionId, newTranscriptNoBr);
-            }
-            else
-            {
-                newWords = HandleAdded(oldWords, newTranscript, newVersionId, newTranscriptNoBr);
-            }*/
+            
             return newWords;
         }
 
@@ -268,68 +251,6 @@ namespace RC_SpeechToText.Services
 
             return timeDouble;
         }
-        private List<Word> HandleEdited(List<Word> oldWords, string newTranscript, Guid newVersionId, List<string> newTranscriptList)
-        {
-            List<Word> newWords = new List<Word>();
-            for (int i = 0; i < newTranscriptList.Count; i++)
-            {
-                newWords.Add(new Word { Term = newTranscriptList[i], Timestamp = oldWords[i].Timestamp, VersionId = newVersionId, Position = i });
-            }
-            return newWords;
-        }
-
-        private List<Word> HandleDeleted(List<Word> oldWords, string newTranscript, Guid newVersionId, List<string> newTranscriptList)
-        {
-            List<Word> newWords = new List<Word>();
-
-            var iterateOld = 0;
-            for (int i = 0; i < newTranscriptList.Count; i++)
-            {
-                for (int j = iterateOld; j < oldWords.Count; j++)
-                {
-                    if (newTranscriptList[i].Equals(oldWords[j].Term, StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        newWords.Add(new Word { Term = newTranscriptList[i], Timestamp = oldWords[j].Timestamp, VersionId = newVersionId, Position = i });
-                        iterateOld = j + 1;
-                        break;
-                    }
-                }
-
-            }
-
-            return newWords;
-        }
-
-        private List<Word> HandleAdded(List<Word> oldWords, string newTranscript, Guid newVersionId, List<string> newTranscriptList)
-        {
-            List<Word> newWords = new List<Word>();
-            var iterateOld = 0;
-			
-            for (int i = 0; i < newTranscriptList.Count; i++)
-            {
-                var currentNewWord = newTranscriptList[i];
-                if (currentNewWord.Equals(oldWords[iterateOld].Term, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    newWords.Add(new Word { Term = currentNewWord, Timestamp = oldWords[iterateOld].Timestamp, VersionId = newVersionId, Position = i });
-                    if (iterateOld != oldWords.Count - 1)
-                    {
-                        iterateOld++;
-                    }
-                }
-                else
-                {
-                    if (iterateOld == 0 || iterateOld == oldWords.Count - 1)
-                    {
-                        newWords.Add(new Word { Term = currentNewWord, Timestamp = oldWords[iterateOld].Timestamp, VersionId = newVersionId, Position = i });
-                    }
-                    else
-                    {
-                        newWords.Add(new Word { Term = currentNewWord, Timestamp = oldWords[iterateOld - 1].Timestamp, VersionId = newVersionId, Position = i });
-                    }
-                }
-            }
-
-            return newWords;
-        }
+        
     }
 }
