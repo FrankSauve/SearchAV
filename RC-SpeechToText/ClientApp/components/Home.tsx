4da3c6dafac08c47cf4c00663f6a64c3cfe33e2b
Raph1105 <Raph1105@hotmail.com>
2019-04-06 14:24:30 -0400

    changed the style of the login page

--- RC-SpeechToText/ClientApp/components/Home.tsx
+++ RC-SpeechToText/ClientApp/components/Home.tsx
@@ -3,18 +3,31 @@ import { RouteComponentProps, Redirect } from 'react-router';
 import Login from './Navbar/Login';
 import auth from '../Utils/auth';
 
-interface State {}
+interface State { }
 
 export class Home extends React.Component<RouteComponentProps<{}>, State> {
     public render() {
-        
+
         return (
             <div>
-                {auth.isLoggedIn() ? <Redirect to="/dashboard"/> : null}
+                {auth.isLoggedIn() ? <Redirect to="/dashboard" /> : null}
+                <section className="is-medium is-light">
+                    <div>
+                        <div className="container">
+                            <h2 className="hometitle">
+                                RBC
+                                <a href="/">
+                                    <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
+                                </a>
+                                Radio Canada
+                            </h2>
+                        </div>
+                    </div>
+                </section>
                 <section className="hero is-small is-dark">
                     <div className="hero-body">
                         <div className="container">
-                            <h2 className="hometitle">
+                            <h2 className="homesubtitle">
                                 CENTRE DES MEMBRES
                             </h2>
                         </div>
@@ -23,11 +36,11 @@ export class Home extends React.Component<RouteComponentProps<{}>, State> {
                 <div className="columns is-centered">
                     <p> L'application SGC Picto souhaite que vous vous identifiez </p>
                     <div className="column is-one-quarter has-text-centered mg-top-30">
-                        <Login/>
+                        <Login />
                     </div>
                 </div>
             </div>
         );
     }
-    
+
 }
--- RC-SpeechToText/ClientApp/components/Navbar/Navbar.tsx
+++ RC-SpeechToText/ClientApp/components/Navbar/Navbar.tsx
@@ -80,14 +80,14 @@ export default class Navbar extends React.Component<RouteComponentProps<{}>, Sta
                                     </div>
                                 </div>
                             </div>
-                            
+
                         </div>
                     </div>
                 </div>
             </div>
         );
     }
-        
+
     public isInFileViewPage() {
         var re = /^.*(FileView).*$/; // Letters only regex
         return re.test(window.location.pathname);
@@ -95,104 +95,93 @@ export default class Navbar extends React.Component<RouteComponentProps<{}>, Sta
 
     public isInLoginPage() {
         var is_root = (location.pathname == "/");
-        return is_root; 
-    }    
+        return is_root;
+    }
     public renderFileViewNav = () => {
         return (
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
-                </a>
-                <div className="navbar-menu">
-                    <div className="navbar-start">
-                        <Link className="navbar-item" to="/dashboard">
-                            <i className="fas fa-angle-left"></i>
-                        </Link>
+            <nav className="navbar has-background-white-smoke container" role="navigation" aria-label="main navigation">
 
-                        <a className="button is-rounded mg-top-10 mg-left-400" onClick={this.showReviewModal}><i className="far fa-envelope mg-right-5 is-link"></i><p className="nav-button is-link"> DEMANDER UNE REVISION</p></a>
-                        <a className="button is-rounded mg-top-10 mg-left-10 " onClick={this.showExportModal}><i className="fas fa-file-export mg-right-5 is-link"></i><p className="nav-button is-link"> EXPORTER</p></a>
-                        <a className="button is-rounded is-link mg-top-10 mg-left-10"><i className="far fa-save mg-right-5"></i> <p className="nav-button"> ENREGISTRER</p></a>
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
+                    </a>
+                    <div className="navbar-menu">
+                        <div className="navbar-start">
+                            <Link className="navbar-item" to="/dashboard">
+                                <i className="fas fa-angle-left"></i>
+                            </Link>
+
+                            <a className="button is-rounded mg-top-10 mg-left-400" onClick={this.showReviewModal}><i className="far fa-envelope mg-right-5 is-link"></i><p className="nav-button is-link"> DEMANDER UNE REVISION</p></a>
+                            <a className="button is-rounded mg-top-10 mg-left-10 " onClick={this.showExportModal}><i className="fas fa-file-export mg-right-5 is-link"></i><p className="nav-button is-link"> EXPORTER</p></a>
+                            <a className="button is-rounded is-link mg-top-10 mg-left-10"><i className="far fa-save mg-right-5"></i> <p className="nav-button"> ENREGISTRER</p></a>
+                        </div>
+
+                        <ExportModal
+                            showModal={this.state.showExportModal}
+                            hideModal={this.hideExportModal}
+                            title={"Exporter un fichier"}
+                            onConfirm={this.hideExportModal}
+                        />
+
+
+                        <SelectReviewerModal
+                            showModal={this.state.showReviewModal}
+                            hideModal={this.hideModal}
+                        />
+
                     </div>
-
-                    <ExportModal
-                        showModal={this.state.showExportModal}
-                        hideModal={this.hideExportModal}
-                        title={"Exporter un fichier"}
-                        onConfirm={this.hideExportModal}
-                    />
-
-
-                    <SelectReviewerModal
-                        showModal={this.state.showReviewModal}
-                        hideModal={this.hideModal}
-                    />
-
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-                );
-            }
-        
+                {this.isInFileViewPage() ? null : auth.isLoggedIn() ? this.renderLoggedIn() : null}
+            </nav >
+        );
+    }
+
     public renderNormalNav = () => {
         return (
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
-                </a>
-                <div className="navbar-menu">
-                    <div className="navbar-start">
-                        <Link className="navbar-item" to="/">
-                            STENO
-                    </Link>
+            <nav className="navbar has-background-white-smoke container" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
+                    </a>
+                    <div className="navbar-menu">
+                        <div className="navbar-start">
+                            <Link className="navbar-item" to="/">
+                                STENO
+                        </Link>
+                        </div>
                     </div>
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+                {this.isInFileViewPage() ? null : auth.isLoggedIn() ? this.renderLoggedIn() : null}
+            </nav>
+
         );
     }
 
     public renderLoginNav = () => {
         return (
-            <div className="navbar-brand">
-                
-                <div className="navbar-menu">
-                    <div className="navbar-start">
-                        <Link className="navbar-item" to="/">
-                            STENO
-                        </Link>
-                        <a className="navbar-item">
-                            <img src="https://vignette.wikia.nocookie.net/logopedia/images/b/b7/Cbc-logo.png/revision/latest/scale-to-width-down/240?cb=20110304223128" width="30" height="30" />
-                        </a>
-                    </div>
-                </div>
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>  
+            <div>
+
+            </div>
         );
     }
-        
+
     public render() {
-        
+
         return (
             <div>
-                <nav className="navbar has-background-white-smoke container" role="navigation" aria-label="main navigation">
+                {this.isInFileViewPage() ? this.renderFileViewNav() : this.isInLoginPage() ? this.renderLoginNav : this.renderNormalNav()}
 
-                    {this.isInFileViewPage() ? this.renderFileViewNav() : this.isInLoginPage() ? this.renderLoginNav : this.renderNormalNav()}
-                    {this.isInFileViewPage() ? null : auth.isLoggedIn() ? this.renderLoggedIn() : null}
-
-                    </nav>
-                </div>
-                )
-            }
-        }
+            </div>
+        )
+    }
+}
