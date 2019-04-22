489200b59ed5413dcacaa0402a7120e8adfbc822
PhilippeKuret <philippekuret@gmail.com>
2019-04-21 21:25:19 -0400

    remove hard coded options

---
+++ RC-SpeechToText/Models/DocumentType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RC_SpeechToText.Models
+{
+    public static class DocumentType
+	{
+		public static string Doc
+		{
+			get
+			{
+				return "doc";
+			}
+		}
+
+		public static string GoogleDoc
+		{
+			get
+			{
+				return "googleDoc";
+			}
+		}
+
+		public static string Srt
+		{
+			get
+			{
+				return "srt";
+			}
+		}
+
+		public static string Video
+		{
+			get
+			{
+				return "video";
+			}
+		}
+	}
+}
